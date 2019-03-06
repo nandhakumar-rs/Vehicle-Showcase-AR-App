4d403030fb4c4803db480c393930c6975133982d
sajansachu127 <sajansanju@gmail.com>
2019-03-06 22:04:13 +0530

    Car models were  designed, code quality has improved, smoke effect vconflict resolved

---
+++ Assets/Scripts/carSelection.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carSelection : MonoBehaviour {
+    //We create an empty list of gamObjects
+    private GameObject[] carList;
+    private int currentCar = 0;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        //Count the child gameObjects
+        carList = new GameObject[transform.childCount];
+
+        //Loop through the chld items and fill the list in correct slot
+        for (int i = 0; i < transform.childCount; ++i) {
+            carList[i] = transform.GetChild(i).gameObject;
+        }
+
+        //Deactivate all gameObjects in list
+        foreach (GameObject gameObj in carList) {
+            gameObj.SetActive(false);
+        }
+
+        //Set initial gameObject to actove
+        if (carList[0]) {
+            carList[0].SetActive(true);
+        }
+
+        
+    }
+
+    public void togglecars (string direction) {
+        carList [currentCar].SetActive(false);
+
+        if(direction == "Right") {
+            currentCar++;
+            if(currentCar > carList.Length -1) {
+                currentCar=0;
+            }
+        }
+        if(direction == "Left") {
+            currentCar--;
+            if(currentCar < 0) {
+                currentCar = carList.Length -1;
+            }
+        }
+
+        //set the current car to be active from the list
+        carList [currentCar].SetActive(true);
+        gameController.currentSelectedCar = carList [currentCar].name;
+        GameObject cloudSystem = Instantiate(Resources.Load("cloud")) as GameObject;
+        ParticleSystem cloudPuff = cloudSystem.GetComponent<ParticleSystem>();
+        cloudPuff.Play();
+        cloudPuff.transform.position = new Vector3(22f, -0.5f, -11.3f);
+        Destroy (cloudSystem, 2f);
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Vehicle-Showcase-AR-app
